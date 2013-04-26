f0d80b284841a7b58073a828f5ef2b3189dfede9
Patrik Jira <patrikjira@gmail.com>
2013-04-26 14:00:36 +0200

    Vanilla app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nanofactory::Application.config.secret_key_base = '5ba8651217070d766417281b82b2010aaff881267404914fb4701b88dd73d8f4adbe290ce8236aeb2ff58c826d3aeb267be92ba3fb92b54f1a34aa6f97f78a82'
