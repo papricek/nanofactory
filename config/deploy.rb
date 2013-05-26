3dcb2946cebf3ee05b08644af3210eebceab8f19
Patrik Jira <patrikjira@gmail.com>
2013-05-26 20:07:19 +0200

    Capified + sqlite

---
+++ config/deploy.rb
@@ -0,0 +1,59 @@
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+require "rvm/capistrano"
+
+# Server
+set :application, "nanofactory"
+set :deploy_to, "/var/www/papricek/#{application}"
+set :user, "papricek"
+set :use_sudo, false
+set :domain, "r01.avatech.cz"
+server domain, :app, :web
+role :db, domain, :primary => true
+
+# scm settings
+set :repository, "git@github.com:papricek/nanofactory.git"
+set :scm, "git"
+set :scm_verbose, true
+set :branch, "master"
+set :keep_releases, 10
+#set :deploy_via, :remote_cache
+
+# Tasks
+after :deploy do
+  deploy.bundle_install
+  site.create_symlinks
+  site.precompile_assets
+  passenger.restart
+  deploy.cleanup
+  deploy.migrate
+end
+
+namespace :deploy do
+  desc "run 'bundle install' to install Bundler's packaged gems for the current deploy"
+  task :bundle_install, :roles => :app do
+    run "cd #{release_path} && bundle install --without test"
+  end
+end
+
+namespace :site do
+  desc "Create or update symlinks "
+  task :create_symlinks do
+    run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{deploy_to}/shared/db/production.sqlite3 #{release_path}/db/production.sqlite3"
+  end
+
+  desc "Precompile assets"
+  task :precompile_assets do
+    run "cd #{release_path}; RAILS_ENV=production bundle exec rake assets:precompile"
+  end
+end
+
+namespace :deploy do
+  task :start do
+  end
+  task :stop do
+  end
+  task :restart, :roles => :app, :except => {:no_release => true} do
+    run "#{try_sudo} touch #{File.join(current_path, 'tmp', 'restart.txt')}"
+  end
+end
